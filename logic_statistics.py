3ff341eed6baef9ad394ae2315a277f306b50c32
Dominik Kaukinen <domkaukinen@gmail.com>
2023-05-12 01:27:19 -0400

    separates out code gen from states

--- logic_statistics.py
+++ logic_statistics.py
@@ -9,7 +9,13 @@ def print_stats(name):
         f"Logic count: {len({l for l in pyrtl.working_block().logic if l.op not in 'wcs'})}")
     logic, _ = pyrtl.area_estimation()
     print(f"Logic area est: {logic}")
+
+
+def write_verilog(name):
     with open(f"{name}.v", 'w') as f:
         pyrtl.output_to_verilog(f)
+
+
+def write_graphviz(name):
     with open(f"{name}.gv", 'w') as f:
         pyrtl.output_to_graphviz(f)
